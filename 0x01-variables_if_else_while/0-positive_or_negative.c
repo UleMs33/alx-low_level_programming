3da360442949b9d457cdd48f28e87b3e229e7aab
Derrick <derrick.ambitho@gmail.com>
2022-09-12 14:41:30 +0000

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,7 +16,7 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-	} 
+	}
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
@@ -27,5 +27,3 @@ int main(void)
 	}
 	return (0);
 }
-		
-	
