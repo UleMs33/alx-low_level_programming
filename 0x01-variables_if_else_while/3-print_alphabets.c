8337bbd7e8b10a8835b3452b93830f6f206573b7
Derrick <derrick.ambitho@gmail.com>
2022-09-12 21:28:56 +0000

    lower and upper case letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/**
+ * main - prints lower case and upper case alphabet letters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char abc[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int count;
+
+	for (count = 0; count < 52; count++)
+	{
+		printf("%c\n",abc);
+	}
+	return (0);
+}
