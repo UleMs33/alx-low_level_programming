f7b01ef6db33539cf0dbac906cba05a9e00a1847
UleMs33 <derrick.ambitho@gmail.com>
2022-09-16 16:40:05 +0300

    Add files via upload
    edited in visual studio code for speed and ease.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ *@n: number of times to print the line
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
