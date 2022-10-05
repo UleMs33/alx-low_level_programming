682016055c835dd4b862d91b5274a1124610f81c
UleMs33 <derrick.ambitho@gmail.com>
2022-10-05 15:37:43 +0300

    Add files via upload

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,23 +1,21 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
+
 /**
- * _strcpy - copies the string pointed to by src,
- * including the terminating null byte, to the
- * buffer pointed to by dest.
- * @dest: destination.
- * @src: source.
- * Return: the pointer to dest.
+ * _strcpy - fuction copies the string pointed
+ * @dest: pointer to copy the string to.
+ * @src: the string to copy to the destination.
+ *
+ * Return: a copy of the src.
  */
 char *_strcpy(char *dest, char *src)
 {
-	int count = 0;
+	int index = 0;
 
-	while (count >= 0)
+	while (*src != '\0')
 	{
-		*(dest + count) = *(src + count);
-		if (*(src + count) == '\0')
-			break;
-		count++;
+		dest[index] = *src;
+		index++;
+		src++;
 	}
 	return (dest);
 }
