c61f26b2aa0a96f0ce81d3d1d9df9c2ff0500b36
Derrick Ambitho <derrick.ambitho@gmail.com>
2023-05-25 07:36:25 -0700

    fixed print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * print_line - draws a straight line in the terminal
- *@n: number of times to print the line
- * Return: Always 0.
+ * print_line - prints straight line n times.
+ * @n: times straight line is printed.
+ * Return: no return.
  */
-
 void print_line(int n)
 {
-	while (n > 0)
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
-		_putchar('_');
-		n--;
+		_putchar(95);
 	}
+	_putchar('\n');
+}
