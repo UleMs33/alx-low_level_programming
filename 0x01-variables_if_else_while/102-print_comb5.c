fd9e11639438e54419fa2c2dacc6590423a2e4d4
Derrick Ambitho <derrick.ambitho@gmail.com>
2023-07-04 06:30:37 -0700

    hurrah

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of two two-digit numbers
+ * main - a simple program that outputs unordered
+ * combinations of two digit integers without duplicates
  *
- * Return: Always 0
+ * Return: 0 on success
  */
 int main(void)
 {
-    int num1, num2;
+	int ifirst;
+	int i;
+	int jfirst;
+	int j;
 
-    for (num1 = 0; num1 <= 99; num1++)
-    {
-        for (num2 = 0; num2 <= 99; num2++)
-        {
-            /* Print first two-digit number */
-            putchar('0' + (num1 / 10));
-            putchar('0' + (num1 % 10));
-
-            /* Print separator space */
-            putchar(' ');
-
-            /* Print second two-digit number */
-            putchar('0' + (num2 / 10));
-            putchar('0' + (num2 % 10));
-
-            /* Print comma and space separator */
-            if (num1 != 99 || num2 != 99)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-
-    putchar('\n');
-
-    return 0;
+	for (ifirst = 48; ifirst < 58; ifirst++)
+	{
+		for (i = 48; i < 58; i++)
+		{
+			j = i + 1;
+			jfirst = ifirst;
+			for (; jfirst < 58; jfirst++)
+			{
+				for (; j < 58; j++)
+				{
+					putchar(ifirst);
+					putchar(i);
+					putchar(' ');
+					putchar(jfirst);
+					putchar(j);
+					if (ifirst != 57 || jfirst != 57 || i != 56 || j != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				j = 48;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
